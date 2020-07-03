ea5602bf8906a2d0eb13a5e19ffb0a16f57e98e1
vincentservio <vincentservio@gmail.com>
2020-07-02 20:46:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submitTask = document.getElementById("create-task-form")
+  const listTask = document.getElementById("tasks")
+
+  submitTask.addEventListener("submit", (event){
+    event.preventDefault()
+    const taskInput = submitTask["new-task-description"].value
+    
+    listTask["new-task-description"].value = 
+  )}
+  listTask.addEventListener("click", (e) => {
+    //e.preventDefault()
+    clickMe(e);
 });
